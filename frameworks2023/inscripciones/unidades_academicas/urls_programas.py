2865291169d375c8b272cf23498863518ac50c1c
Adalberto Cerrillo <34142734@uaz.edu.mx>
2023-02-21 11:42:14 -0600

    CRUD Programas terminado

--- frameworks2023/inscripciones/unidades_academicas/urls_programas.py
+++ frameworks2023/inscripciones/unidades_academicas/urls_programas.py
@@ -4,4 +4,6 @@ from unidades_academicas import views
 urlpatterns = [
     path('', views.lista_programas, name='lista_programas'),
     path('Agregar/', views.agregar_programa, name='agregar_programa'),
+    path('Eliminar/<int:clave>', views.eliminar_programa, name= 'eliminar_programa'),
+    path('Editar/<int:clave>', views.editar_programa, name= 'editar_programa'),
 ]
--- frameworks2023/inscripciones/unidades_academicas/views.py
+++ frameworks2023/inscripciones/unidades_academicas/views.py
@@ -16,3 +16,19 @@ def agregar_programa(request):
         form = FormPrograma()
     contexto = {'form': form}
     return render(request, 'agregar_programas.html', contexto)
+
+def eliminar_programa(request, clave):
+    ProgramaAcademico.objects.get(clave = clave).delete()
+    return redirect('lista_programas')
+
+def editar_programa(request, clave):
+    programa = ProgramaAcademico.objects.get(clave = clave)
+    if request.method == 'POST':
+        form = FormPrograma(request.POST, instance = programa)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_programas')
+    else:
+        form = FormPrograma(instance = programa)
+    contexto = {'form': form}
+    return render(request, 'editar_programa.html', contexto)
