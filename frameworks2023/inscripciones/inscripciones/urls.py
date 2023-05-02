c1edf340c9f83722b2adba0aed387ece47bf1f14
Adalberto Cerrillo <34142734@uaz.edu.mx>
2023-05-02 11:29:11 -0500

    Agregar menus

--- frameworks2023/inscripciones/inscripciones/urls.py
+++ frameworks2023/inscripciones/inscripciones/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from materias.views import Bienvenida, Bienvenida2
+from usuarios.views import LoginView
 
 urlpatterns = [
+    path('', LoginView.as_view(), name='login'),
     path('admin/',admin.site.urls),
     path('programas/', include('unidades_academicas.urls_programas')),
     path('unidades/', include('unidades_academicas.urls_unidades')),
     path('materias/', include('materias.urls')),
     path('horarios/', include('horarios.urls')),
-    path('', Bienvenida.as_view(), name = 'home'),
+    path('home/', Bienvenida.as_view(), name = 'home'),
     path('dashboard/', Bienvenida2.as_view(), name = 'home2'),
     path('usuarios/', include('usuarios.urls'))
 ]
--- frameworks2023/inscripciones/usuarios/urls.py
+++ frameworks2023/inscripciones/usuarios/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('registrar-docente/', views.RegistrarDocente.as_view(), name='registrarDocente'),
     path('registrar-docente2/', views.RegistrarDocente2.as_view(), name='registrarDocente2'),
